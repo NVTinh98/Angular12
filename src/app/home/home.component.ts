768537d89ea49c316df58550b97c975ba2227319
NVTinh98 <tinhnguyen13198@gmail.com>
2023-08-11 15:08:32 +0700

    ngForApD

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HOMEComponent {
-  public name = 'Tỉnh';
+  public name = 'LÊ VĂN ĐỨC';
   public age = 15;
   public traiCay = ['Táo','Nho','Cam','Quyt'];
+  public traiCay2=[
+    {ten: 'Tao', gia: 12, haGia: '20%'},
+    {ten: 'Nho', gia: 10, haGia: '10%'},
+    {ten: 'Cam', gia: -5, haGia: 0},
+    {ten: 'Quýt', gia: 5, haGia: '30%'},
+  ];
 
   public ngOnInit(): void{
     console.log('trai cay = ', this.traiCay)
+    console.log('trai cay = ', this.traiCay2)
   }
 }
