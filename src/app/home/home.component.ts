6e21633315c266b1dc6e0523dae2265be0f1c000
NVTinh98 <tinhnguyen13198@gmail.com>
2023-08-14 16:01:16 +0700

    ngForCombobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,8 +16,56 @@ export class HOMEComponent {
     {ten: 'Quýt', gia: 5, haGia: '30%'},
   ];
 
+public districts: string[] = ['Quận Huyện'];
+public vietnamData = [
+  {city: 'Tỉnh Thành Phố', district:['Quận Huyện']},
+  {
+    city: 'Hưng Yên',
+  district: [
+    'Thành phố Hưng Yên',
+    'Thị Xã Mỹ Hào',
+    'Huyện Khoái Châu',
+    'Huyện Yên Mỹ',
+    'Huyện Phù Cừ',
+    'Huyện Ân Thi',
+    'Huyện Tiên Lữ'
+  ]},
+  {
+    city:'Hà Nội',
+    district:[
+      'Quận Long Biên',
+      'Quận Hai Bà Trưng',
+      'Quận Cầu Tõm',
+      'Quận Nam Mộ Dung',
+      'Quận Bắc Tiêu Phong',
+      'Quận Đức Tuần Châu',
+    ]
+  }
+
+];
+
   public ngOnInit(): void{
-    console.log('trai cay = ', this.traiCay)
-    console.log('trai cay = ', this.traiCay2)
+    // console.log('trai cay = ', this.traiCay)
+    // console.log('trai cay = ', this.traiCay2)
+    // console.log('thanhpho = ', this.cities)
+    console.log('vietnamData', this.vietnamData);
+  }
+
+  public changeCity(event: any){
+    const city = event.target.value;
+    console.log('event', city);
+
+    if(!city){
+      return;
+    }
+
+    //Cách 1
+    // const search = this.vietnamData.filter((data) => data.city === city);
+    // console.log('search', search)
+    // if(search && search.length > 0){
+    //   this.districts = search[0].district;
+
+    //Cách 2
+    this.districts = this.vietnamData.find(data => data.city === city)?.district || [];
   }
 }
